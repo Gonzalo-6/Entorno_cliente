624a2a2017d13d2f944f13629d4884b4baf22055
Gonzalo-6 <g-arranz@hotmail.com>
2026-04-08 10:25:38 +0200

    clase

--- clase_7_4/app.js
+++ clase_7_4/app.js
@@ -1,18 +1,19 @@
 const productos = [
-    { id: 1, nombre: "teclado", precio: 20 },
-    { id: 2, nombre: "monitor", precio: 200 },
-    { id: 3, nombre: "raton", precio: 50 },
+    { id: 1, nombre: "teclado", precio: 20, categoria: "perifericos" },
+    { id: 2, nombre: "monitor", precio: 200, categoria: "pantallas" },
+    { id: 3, nombre: "raton", precio: 50, categoria: "perifericos" },
 ];
 let indice = 0;
 
 function crearTarjeta (producto) {
     const tarjeta = document.createElement("div");
-    tarjeta.setAttribute("class", "tarjeta");
+    tarjeta.setAttribute("class", "card");
 
     const nombre = document.createElement("h3");
     nombre.textContent = producto.nombre;
     const precio = document.createElement("p");
     precio.textContent = producto.precio;
+   
 
     const boton = document.createElement("button");
     boton.textContent = "Agregar al carrito";
@@ -31,7 +32,7 @@ function crearTarjeta (producto) {
     return tarjeta;
 
 }
-document.querySelector("#btnAgregar").addEventListener("click", () => {
+document.querySelector("#boton").addEventListener("click", () => {
     if (indice >= productos.length) return ;
     const tarjeta_unica = crearTarjeta(productos[indice]);
     document.querySelector("#catalogo").appendChild(tarjeta_unica);

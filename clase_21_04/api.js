ec46e399315b17a7b7075dd47df70527baade197
Gonzalo-6 <g-arranz@hotmail.com>
2026-04-22 10:19:55 +0200

    clase

---
+++ clase_21_04/api.js
@@ -0,0 +1,18 @@
+const API = "htttps://jsonplacerholder.typicode.com/post";
+
+async function obtenerPosts() {
+    try {
+        const res = await fetch(API);
+        if (!res.ok) {
+            throw new Error(`Error HTTP: ${res.status}`);
+        }else {
+            const posts = await res.json();
+            console.log("Total de posts:", posts.length);
+            consologe.log("Primeros 2:", posts.slice(0, 2));
+            return posts;
+        }
+    }catch (error) {
+        console.error("Error al obtener posts:", error.message);
+        return[];
+    }
+}
